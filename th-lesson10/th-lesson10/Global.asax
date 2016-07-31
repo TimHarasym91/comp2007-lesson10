a39d9d8e211bb66c2c0f80f4e780f812d9f6af5c
TimHarasym <timharasym@gmail.com>
2016-07-31 13:23:04 -0400

    INITIAL COMMIT

---
+++ th-lesson10/th-lesson10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="th_lesson10.MvcApplication" Language="C#" %>
---
+++ th-lesson10/th-lesson10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(th_lesson10.Startup))]
+namespace th_lesson10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
