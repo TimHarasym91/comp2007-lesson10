9d83e84859ce3478a8c7a7d90c7fbe8520f917c5
TimHarasym <timharasym@gmail.com>
2016-07-31 13:46:00 -0400

    Initial changes

--- th-lesson10/th-lesson10/Controllers/HomeController.cs
+++ th-lesson10/th-lesson10/Controllers/HomeController.cs
@@ -8,23 +8,13 @@ namespace th_lesson10.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Home/
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
